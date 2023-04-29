abde716d0a152054ba924016af7057a409c21e15
aneurysm <aneurysmjs@outlook.com>
2023-04-29 23:51:27 +0200

    feat(apiumhub/utils): add formatMilliseconds

---
+++ packages/utils/src/formatMilliseconds/formatMilliseconds.test.ts
@@ -0,0 +1,7 @@
+import formatMilliseconds from './formatMilliseconds';
+
+describe('formatMilliseconds', () => {
+  it('formats milliseconds to HH:mm:ss', () => {
+    expect(formatMilliseconds(3672000)).toBe('01:01:21');
+  });
+});
